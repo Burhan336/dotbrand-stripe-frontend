7e0c93fc5c208d705a1b4a6b81eababa78a311ed
harisejaz2206 <harisejaz2206@gmail.com>
2023-11-12 15:35:35 +0500

    form flow completed. Onto burhan for frontend changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,6 @@ import InitiateCheckout from './components/stripe/InitiateCheckout';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import axios from "axios";
-import PayPalPlans from './components/paypal/PayPalPlans';
-import PayPalInitiateCheckout from './components/paypal/PayPalInitiateCheckout';
-import PayPalConfirmPlan from './components/paypal/PayPalConfirmPlan';
-import PayPalSuccess from './components/paypal/PayPalSuccess';
-import PayPalFailed from './components/paypal/PayPalFailed';
-import PaypalOneTimeButton from './components/paypal-onetime/PaypalButton';
 import Homepage from './pages/HomePage';
 import ContactUs from './pages/ContactUs';
 import PricingPlans from './pages/PricingPlans';
@@ -32,27 +26,21 @@ function App() {
     <Router>
       <Elements stripe={stripePromise}>
         <div>
-        <Navbar />
-        <Routes>
-         <Route path="/" element={<Homepage />} />
-         <Route path="/purchase" element={<SetupForm />} />
-          <Route path="/features" element={<ProductFeatures />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/paypal" element={<PayPal />} />
-          <Route path="/stripe" element={<Stripe />} />
-          <Route path="/plans" element={<Plans />} />
-          <Route path="/confirm-plan" element={<ConfirmPlan />} />
-          <Route path="/initiate-checkout" element={<InitiateCheckout />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/failed" element={<Failed />} />
-          <Route path="/paypal-plans" element={<PayPalPlans />} />
-          <Route path="/paypal-initiate-checkout" element={<PayPalInitiateCheckout />} />
-          <Route path="/paypal-confirm-plan" element={<PayPalConfirmPlan />} />
-          <Route path="/paypal-success" element={<PayPalSuccess />} />
-          <Route path="/paypal-failed" element={<PayPalFailed />} />
-          <Route path="/paypal-onetime" element={<PaypalOneTimeButton />} />
-        </Routes>
-        <Footer/>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/purchase" element={<SetupForm />} />
+            <Route path="/features" element={<ProductFeatures />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/paypal" element={<PayPal />} />
+            <Route path="/stripe" element={<Stripe />} />
+            <Route path="/plans" element={<Plans />} />
+            <Route path="/confirm-plan" element={<ConfirmPlan />} />
+            <Route path="/initiate-checkout" element={<InitiateCheckout />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/failed" element={<Failed />} />
+          </Routes>
+          <Footer />
         </div>
       </Elements>
     </Router>
--- src/pages/SetupForm.tsx
+++ src/pages/SetupForm.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Toast } from "../utils/toast";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SetupForm: React.FC = () => {
+  const navigate = useNavigate();
+
   const initialValues = {
     storeName: "",
     multiAdminName: "",
     multiAdminEmail: "",
     multiAdminPassword: "",
-    contact: "",
-    address: "",
+
   };
 
   const validationSchema = Yup.object({
@@ -19,155 +25,131 @@ const SetupForm: React.FC = () => {
       .email("Invalid email address")
       .required("Email is required"),
     multiAdminPassword: Yup.string().required("Password is required"),
-    contact: Yup.string().required("Contact is required"),
-    address: Yup.string().required("Address is required"),
   });
 
-  const handleSubmit = (values: any, { resetForm }: any) => {
-    // You can handle form submission here, e.g., send the data to your server.
+  const handleSubmit = async (values: any, { resetForm }: any) => {
+    try {
+      // Make a POST request to your API
+      await axios.post("http://localhost:7979/api/submit-form", values);
 
-    // Reset the form after submission
-    resetForm();
+      // Reset the form after successful submission
+      resetForm();
+      alert("Your request has been received. It is currently pending payment verification. Upon successful verification, your multi-chain account will be created.");
+
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      // Handle errors as needed
+    } finally {
+      Toast.fire({
+        icon: "success",
+        title: "Request submitted",
+      });
+      navigate("/confirm-plan")
+    }
   };
 
   return (
-    <div className="container mx-auto p-4 max-w-md bg-white rounded shadow-lg flex flex-col items-center mt-4 mb-4">
-      <h1 className="text-xl font-semibold mb-4">DotBrand Application Setup</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form className="w-full">
-          <div className="mb-4">
-            <label
-              htmlFor="storeName"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Store Name
-            </label>
-            <Field
-              type="text"
-              id="storeName"
-              name="storeName"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="storeName"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
+    <div>
+      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} />
+      <div className="container mx-auto p-4 max-w-md bg-white rounded shadow-lg flex flex-col items-center mt-4 mb-4">
+        <h1 className="text-xl font-semibold mb-4">DotBrand Application Setup</h1>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className="w-full">
+            <div className="mb-4">
+              <label
+                htmlFor="storeName"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Store Name
+              </label>
+              <Field
+                type="text"
+                id="storeName"
+                name="storeName"
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+              />
+              <ErrorMessage
+                name="storeName"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="multiAdminName"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Multi-Admin Name
-            </label>
-            <Field
-              type="text"
-              id="multiAdminName"
-              name="multiAdminName"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="multiAdminName"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="multiAdminName"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Multi-Admin Name
+              </label>
+              <Field
+                type="text"
+                id="multiAdminName"
+                name="multiAdminName"
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+              />
+              <ErrorMessage
+                name="multiAdminName"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="multiAdminEmail"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Multi-Admin Email
-            </label>
-            <Field
-              type="email"
-              id="multiAdminEmail"
-              name="multiAdminEmail"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="multiAdminEmail"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="multiAdminEmail"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Multi-Admin Email
+              </label>
+              <Field
+                type="email"
+                id="multiAdminEmail"
+                name="multiAdminEmail"
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+              />
+              <ErrorMessage
+                name="multiAdminEmail"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="multiAdminPassword"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Multi-Admin Password
-            </label>
-            <Field
-              type="password"
-              id="multiAdminPassword"
-              name="multiAdminPassword"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="multiAdminPassword"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="multiAdminPassword"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Multi-Admin Password
+              </label>
+              <Field
+                type="password"
+                id="multiAdminPassword"
+                name="multiAdminPassword"
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+              />
+              <ErrorMessage
+                name="multiAdminPassword"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="contact"
-              className="block text-sm font-medium text-gray-600"
+            <button
+              type="submit"
+              className="text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center flex items-center"
             >
-              Contact
-            </label>
-            <Field
-              type="text"
-              id="contact"
-              name="contact"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="contact"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="block text-sm font-medium text-gray-600"
-            >
-              Address
-            </label>
-            <Field
-              as="textarea"
-              id="address"
-              name="address"
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
-            />
-            <ErrorMessage
-              name="address"
-              component="div"
-              className="text-red-500 text-xs"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center flex items-center"
-          >
-            Submit
-          </button>
-        </Form>
-      </Formik>
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </div>
+
   );
 };
 
